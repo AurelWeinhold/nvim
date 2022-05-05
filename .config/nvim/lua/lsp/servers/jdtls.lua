4d6b417666de2101a4fa5ec7172c26a496141008
Aurel Weinhold <aurel@weinhold.org>
2022-05-05 17:30:07 +0200

    Move configuration into the dict
    NOTE: Nothing really has changed, the code is just in a different place

--- .config/nvim/lua/lsp/servers/jdtls.lua
+++ .config/nvim/lua/lsp/servers/jdtls.lua
@@ -82,55 +82,56 @@ function M.setup()
     local workspace_folder = home .. "/Documents/workspace" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
     local config = {
         flags = {
-          allow_incremental_sync = true,
+            allow_incremental_sync = true,
         };
         capabilities = capabilities,
         on_attach = on_attach,
-    }
-
-     config.settings = {
-        ['java.format.settings.url'] = home .. "/.config/nvim/language-servers/java-google-formatter.xml",
-        ['java.format.settings.profile'] = "GoogleStyle",
-        java = {
-          signatureHelp = { enabled = true };
-          contentProvider = { preferred = 'fernflower' };
-          completion = {
-            favoriteStaticMembers = {
-              "org.hamcrest.MatcherAssert.assertThat",
-              "org.hamcrest.Matchers.*",
-              "org.hamcrest.CoreMatchers.*",
-              "org.junit.jupiter.api.Assertions.*",
-              "java.util.Objects.requireNonNull",
-              "java.util.Objects.requireNonNullElse",
-              "org.mockito.Mockito.*"
-            }
-          };
-          sources = {
-            organizeImports = {
-              starThreshold = 9999;
-              staticStarThreshold = 9999;
-            };
-          };
-          codeGeneration = {
-            toString = {
-              template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
-            }
-          };
-          --configuration = {
-            --runtimes = {
-              --{
+        settings = {
+            ['java.format.settings.url'] = home .. "/.config/nvim/language-servers/java-google-formatter.xml",
+            ['java.format.settings.profile'] = "GoogleStyle",
+            java = {
+                signatureHelp = { enabled = true };
+                contentProvider = { preferred = 'fernflower' };
+                completion = {
+                    favoriteStaticMembers = {
+                        "org.hamcrest.MatcherAssert.assertThat",
+                        "org.hamcrest.Matchers.*",
+                        "org.hamcrest.CoreMatchers.*",
+                        "org.junit.jupiter.api.Assertions.*",
+                        "java.util.Objects.requireNonNull",
+                        "java.util.Objects.requireNonNullElse",
+                        "org.mockito.Mockito.*"
+                    }
+                };
+                sources = {
+                    organizeImports = {
+                        starThreshold = 9999;
+                        staticStarThreshold = 9999;
+                    };
+                };
+                codeGeneration = {
+                    toString = {
+                        template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
+                    }
+                };
+                --configuration = {
+                --runtimes = {
+                --{
                 --name = "java-17-openjdk",
                 --path = "/usr/lib/jvm/java-17-openjdk",
-              --},
-            --}
-          --};
-        };
+                --},
+                --}
+                --};
+            };
+        },
+        cmd = {
+            'java-lsp', -- custom script
+            workspace_dir
+        },
+        on_init = function(client, _)
+            client.notify('workspace/didChangeConfiguration', { settings = config.settings })
+        end
     }
-    config.cmd = {'java-lsp', workspace_folder}
-    config.on_attach = on_attach
-    config.on_init = function(client, _)
-        client.notify('workspace/didChangeConfiguration', { settings = config.settings })
-    end
 
     require('jdtls').start_or_attach(config)
 end
