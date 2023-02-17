8becccd905b7ad266be4832469d2a4149caff418
Aurel Weinhold <aurel@weinhold.org>
2023-02-17 15:42:10 +0100

    Add pyright language server

--- .config/nvim/lua/lsp/config.lua
+++ .config/nvim/lua/lsp/config.lua
@@ -34,7 +34,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- loop over servers, that don't require special configuration
-local servers = { "vimls", "texlab", "bashls", "cmake", "tsserver" }
+local servers = { "vimls", "texlab", "bashls", "cmake", "tsserver", "pyright" }
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup {
 		on_attach = on_attach,
