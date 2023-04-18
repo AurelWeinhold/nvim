a32e02c5dae1f681e8dbafe2c89f7ffb48fda637
Aurel Weinhold <aurel@weinhold.org>
2023-04-18 14:46:04 +0200

    Change note creation filename to include the actual note name

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -9,6 +9,20 @@ require("obsidian").setup({
 	completion = {
 		nvim_cmp = true,
 	},
+	note_id_func = function(title)
+		-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+		local suffix = ""
+		if title ~= nil then
+			-- If title is given, transform it into valid file name.
+			suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+		else
+			-- If title is nil, just add 4 random uppercase letters to the suffix.
+			for _ = 1, 4 do
+				suffix = suffix .. string.char(math.random(65, 90))
+			end
+		end
+		return  suffix .. "-" .. tostring(os.time())
+	end
 })
 
 -- gf or obsidians gf if fitting
