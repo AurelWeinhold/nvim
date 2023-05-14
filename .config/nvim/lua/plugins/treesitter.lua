94e0ce85c75fc1f495b2c9d08e4abd07aa9f2cab
Aurel Weinhold <aurel@weinhold.org>
2023-05-14 16:03:28 +0200

    Add shortcuts for ts incremental selection

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,13 @@ require('nvim-treesitter.configs').setup {
 	ensure_installed = { "c", "cpp", "latex", "java" },
 	highlight = { enable = true },
 	indent = { enable = false },
-	incremental_selection = { enable = true }
+	incremental_selection = { enable = true },
+	keymaps = {
+		init_selection = "gnn",
+		node_incremental = "grn",
+		scope_incremental = "grc",
+		node_decremental = "grm",
+	},
 }
 
 -- parsers
