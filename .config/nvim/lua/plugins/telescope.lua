e94f868500dceba3538a545b672d593b1d2b4b0f
Aurel Weinhold <aurel@weinhold.org>
2024-01-26 12:12:17 +0100

    Add telescope bindings to access tags and replace spell suggest with a telescope picker

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -34,5 +34,9 @@ require('telescope').load_extension('fzf')
 -- mappings
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>t', builtin.tags, {})
+vim.keymap.set('n', '<leader>ft', builtin.current_buffer_tags, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- replace default z= with a telescope picker
+vim.keymap.set('n', 'z=', builtin.spell_suggest, {})
