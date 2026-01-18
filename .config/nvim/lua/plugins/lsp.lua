ea0f4dd90d46f5b5a9bf4e8a49f80852d0d94d67
Aurel Weinhold <aurel@weinhold.org>
2026-01-18 15:59:18 +0100

    Remove deprecated vim.diagnostic.goto_{prev,next}
    The binding is already the default behavior now.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,8 +13,6 @@ local ensure_installed = {
 local lsp_conf = function()
 	local opts = { noremap=true, silent=true }
 	vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
-	vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-	vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 	vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
 	local on_attach = function(client, bufnr)
